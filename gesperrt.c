ffc7b28ab670be1c2e5b9621287b0cb058e6aaa6
Michael Roe <michael.roe@github.com>
2026-03-26 22:16:22 +0000

    Add gesperrt.c

---
+++ gesperrt.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+#include <wchar.h>
+#include <wctype.h>
+
+static wchar_t buff[1024];
+
+int main(int argc, char **argv)
+{
+int len;
+int gesperrt;
+wchar_t *cp;
+
+  setlocale(LC_ALL, getenv("LANG"));
+
+  gesperrt = 0;
+
+  while (fgetws(buff, sizeof(buff), stdin) != 0)
+  {
+    len = wcslen(buff);
+    if (buff[len - 1] == '\n')
+    {
+      buff[len - 1] = '\0';
+      len--;
+    }
+    cp = buff;
+    while (*cp)
+    {
+      if (gesperrt)
+      {
+        if (wcsncmp(cp, L"</g>", 4) == 0)
+        {
+          cp += 4;
+          gesperrt = 0;
+          wprintf(L"\n");
+        }
+        else
+        {
+          wprintf(L"%lc", *cp);
+          cp++;
+        }
+      }
+      else
+      {
+        if (wcsncmp(cp, L"<g>", 3) == 0)
+        {
+          cp += 3;
+          gesperrt = 1;
+        }
+        else
+        {
+          cp++;
+        }
+      }
+    }
+    if (gesperrt)
+    {
+      wprintf(L" ");
+    }
+  }
+  return 0;
+}
